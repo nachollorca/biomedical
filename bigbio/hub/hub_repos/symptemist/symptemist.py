f80f97cc2f65a0a6d2e0b84bc395ef4462c378ff
Ignacio Rodriguez <Ignacio.Rodriguez@athena.dhclab.i.hpi.de>
2023-09-15 12:13:01 +0200

    support subtask2-linking

--- bigbio/hub/hub_repos/symptemist/symptemist.py
+++ bigbio/hub/hub_repos/symptemist/symptemist.py
@@ -36,18 +36,18 @@ _DESCRIPTION = """\
 The SympTEMIST corpus is a collection of 1,000 clinical case reports in Spanish annotated with symptoms, signs and findings mentions and normalized to SNOMED CT. The texts belong to the SPACCC corpus and are the same ones used in SympTEMIST and MedProcNER, making the annotations complementary for medical entity recognition.
 """
 
-_HOMEPAGE = "https://zenodo.org/record/8223654"
+_HOMEPAGE = "https://temu.bsc.es/symptemist/"
 
 _LICENSE = 'CC_BY_4p0'
 
 _URLS = {
-    _DATASETNAME: "https://zenodo.org/record/8223654/files/symptemist_train.zip?download=1",
+    _DATASETNAME: "https://zenodo.org/record/8345014/files/symptemist-train_all_subtasks%2Bgazetteer%2Bmultilingual_230914.zip?download=1",
 }
 
 _SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.NAMED_ENTITY_DISAMBIGUATION]
 
-_SOURCE_VERSION = "0.0.0"
-_BIGBIO_VERSION = "1.0.0"
+_SOURCE_VERSION = "1.0.0"
+_BIGBIO_VERSION = "2.0.0"
 
 
 class SymptemistDataset(datasets.GeneratorBasedBuilder):
@@ -66,13 +66,13 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
             schema="source",
             subset_id="symptemist_entities",
         ),
-        #BigBioConfig(
-        #    name="symptemist_linking_source",
-        #    version=SOURCE_VERSION,
-        #    description="SympTEMIST (subtrack 2: linking) source schema",
-        #    schema="source",
-        #    subset_id="symptemist_linking",
-        #),
+        BigBioConfig(
+            name="symptemist_linking_source",
+            version=SOURCE_VERSION,
+            description="SympTEMIST (subtrack 2: linking) source schema",
+            schema="source",
+            subset_id="symptemist_linking",
+        ),
         BigBioConfig(
             name="symptemist_entities_bigbio_kb",
             version=BIGBIO_VERSION,
@@ -80,13 +80,13 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
             schema="bigbio_kb",
             subset_id="symptemist_entities",
         ),
-        #BigBioConfig(
-        #    name="symptemist_linking_bigbio_kb",
-        #    version=BIGBIO_VERSION,
-        #    description="SympTEMIST (subtrack 2: linking) BigBio schema",
-        #    schema="bigbio_kb",
-        #    subset_id="symptemist_linking",
-        #),
+        BigBioConfig(
+            name="symptemist_linking_bigbio_kb",
+            version=BIGBIO_VERSION,
+            description="SympTEMIST (subtrack 2: linking) BigBio schema",
+            schema="bigbio_kb",
+            subset_id="symptemist_linking",
+        ),
     ]
 
     DEFAULT_CONFIG_NAME = "symptemist_entities_source"
@@ -133,7 +133,7 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
         """Returns SplitGenerators."""
         urls = _URLS[_DATASETNAME]
         data_dir = dl_manager.download_and_extract(urls)
-        base_bath = Path(data_dir) / "symptemist_train"
+        base_bath = Path(data_dir) / "symptemist-train_all_subtasks+gazetteer+multilingual_230914"
         track = self.config.subset_id.split('_')[1]
                 
         return [
@@ -163,34 +163,32 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
     ) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
 
-        print("track:", track, "split:", split)
-
         tsv_files = {
             ('entities', 'train'): [
-                base_bath / "subtask1-ner" / "tsv" / "symptemist_tsv_train_subtask1.tsv"
+                base_bath / "symptemist_train" / "subtask1-ner" / "tsv" / "symptemist_tsv_train_subtask1.tsv"
             ],
-            ('entities', 'test'): [
-                base_bath / "test_annotated" / "subtrack1_entities" / "symptemist_subtrack1_test_mentions.tsv"
-            ],
+            #('entities', 'test'): [
+            #    base_bath / "test_annotated" / "subtrack1_entities" / "symptemist_subtrack1_test_mentions.tsv"
+            #],
             ('linking', 'train'): [
-                base_bath / "training" / "subtrack2_linking" / "symptemist_subtrack2_training1_linking.tsv",
-                base_bath / "training" / "subtrack2_linking" / "symptemist_subtrack2_training2_linking.tsv",
+                base_bath / "symptemist_train" / "subtask2-linking" / "symptemist_tsv_train_subtask2.tsv"
             ],
-            ('linking', 'test'): [
-                base_bath / "test_annotated" / "subtrack2_linking" / "symptemist_subtrack2_test_linking.tsv"
-            ],       
+            #('linking', 'test'): [
+            #    base_bath / "test_annotated" / "subtrack2_linking" / "symptemist_subtrack2_test_linking.tsv"
+            #],
         }
 
         entity_mapping_files = tsv_files[(track, split)]
 
         if split == "train":
-            text_files_dir = base_bath / "subtask1-ner" / "txt"
+            text_files_dir = base_bath / "symptemist_train" / "subtask1-ner" / "txt"
         #elif split == "test":
         #    text_files_dir = base_bath / "test_annotated" / "text_files"
         
+        # keep this in case more files are added later
         entities_mapping = pd.concat([pd.read_csv(file, sep="\t") for file in entity_mapping_files])
         entity_file_names = entities_mapping["filename"].unique()
-
+        
         for uid, filename in enumerate(entity_file_names):
             text_file = text_files_dir / f"{filename}.txt"
 
@@ -219,7 +217,7 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
             entities = []
             for row in entities_df.itertuples(name="Entity"):
                 entity = {
-                    "id": f"{uid}_{row.filename}_{row.Index}_entity_id_{row.ann_id}",
+                    "id": f"{uid}_{row.filename}_{row.Index}_entity_id",#_{row.ann_id}",
                     "type": row.label,
                     "text": [row.text],
                     "offsets": [[row.start_span, row.end_span]],
@@ -229,7 +227,7 @@ class SymptemistDataset(datasets.GeneratorBasedBuilder):
                     entity["semantic_relations"] = []
                     if self.config.subset_id == "symptemist_linking":
                         entity["concept_codes"] = row.code.split("+")
-                        entity["semantic_relations"] = row.semantic_rel.split("+")
+                        entity["semantic_relations"] = row.sem_rel.split("+")
 
                 elif self.config.schema == "bigbio_kb":
                     if self.config.subset_id == "symptemist_linking":
